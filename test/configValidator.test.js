213f22cf1ce037f61d1204fa910c91b4ba5d733d
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2020-06-05 11:39:12 +0530

    Add more config validator tests

--- test/configValidator.test.js
+++ test/configValidator.test.js
@@ -13,7 +13,15 @@ describe('should validate configurations through configValidator', () => {
             apiPath: '/test-status',
             mode: "ON_REQUEST",
             consumedServicesAsyncMode: true,
-            consumedServices: {},
+            consumedServices: {
+                mockId: {
+                    serviceName: 'mock service',
+                    healthCheckUrl: '/',
+                    requestMethod: 'GET',
+                    expectedResponseStatus: '200',
+                    isRequired: true
+                }
+            },
             apis: {}
           }
     })
@@ -34,7 +42,6 @@ describe('should validate configurations through configValidator', () => {
 
     it ('should set default path if custom config does not have path', () => {
         customConfig.apiPath = "";
-        expect(customConfig.apiPath).is.empty;
         const config = configValidator(customConfig);
         expect(config.apiPath).is.not.empty;
         expect(config.apiPath).is.equal(defaultConfig.apiPath)
@@ -49,7 +56,6 @@ describe('should validate configurations through configValidator', () => {
 
     it ('should set default mode if custom config does not have consumedServicesAsyncMode', () => {
         customConfig.consumedServicesAsyncMode = undefined;
-        expect(customConfig.consumedServicesAsyncMode).is.undefined;
         const config = configValidator(customConfig);
         expect(config.consumedServicesAsyncMode).is.not.undefined;
         expect(config.consumedServicesAsyncMode).is.equal(defaultConfig.consumedServicesAsyncMode)
@@ -57,7 +63,6 @@ describe('should validate configurations through configValidator', () => {
 
     it ('should not set default consumedServicesAsyncMode if custom config has a valid consumedServicesAsyncMode', () => {
         customConfig.consumedServicesAsyncMode = false;
-        expect(customConfig.consumedServicesAsyncMode).is.not.undefined;
         const config = configValidator(customConfig);
         expect(config.consumedServicesAsyncMode).is.not.undefined;
         expect(config.consumedServicesAsyncMode).is.equal(customConfig.consumedServicesAsyncMode)
@@ -81,7 +86,7 @@ describe('should validate configurations through configValidator', () => {
 
         it ("should set valid response.statusCodes if custom config have a invalid property", () => {
             customConfig.response = { statusCodes: 'foo' }
-            expect(customConfig.response.statusCodes).is.not.undefined;
+            // TODO: Check this type of assert
             expect(typeof customConfig.response.statusCodes).is.not.equal(typeof true)
             const config = configValidator(customConfig);
             expect(config.response.statusCodes).is.not.undefined;
@@ -89,4 +94,132 @@ describe('should validate configurations through configValidator', () => {
             expect(config.response.statusCodes).is.equal(defaultConfig.response.statusCodes)
         })
     })
+
+    describe("should validate consumed services configurations", () => {
+        it ('should set default config if custom config does not have valid consumedServices config', () => {
+            customConfig.consumedServices = undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices).is.not.undefined;
+            expect(config.consumedServices).is.empty
+        });
+
+        it ('should not set default config if custom config has valid consumedServices config', () => {
+            customConfig.consumedServices = { mockId: {}};
+            const config = configValidator(customConfig);
+            expect(config.consumedServices).is.not.empty;
+            expect(config.consumedServices).is.equal(customConfig.consumedServices)
+            expect(config.consumedServices).is.not.equal(defaultConfig.consumedServices)
+        });
+
+        it ('should set default serviceName if custom config does not have a valid property', () => {
+            customConfig.consumedServices.mockId.serviceName = undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.serviceName).is.not.undefined;
+            expect(config.consumedServices.mockId.serviceName).is.equal(defaultConfig.consumedServices.defaultServiceId.serviceName)
+        });
+
+        it ('should not set default serviceName if custom config has valid property', () => {
+            expect(customConfig.consumedServices.mockId.serviceName).is.not.undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.serviceName).is.not.undefined;
+            expect(config.consumedServices.mockId.serviceName).is.equal(customConfig.consumedServices.mockId.serviceName)
+            expect(config.consumedServices.mockId.serviceName).is.not.equal(defaultConfig.consumedServices.defaultServiceId.serviceName)
+        });
+
+        it ('should set service.isValid=False if custom config does not have a valid healthCheckURL', () => {
+            customConfig.consumedServices.mockId.healthCheckUrl = undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.healthCheckUrl).is.undefined;
+            expect(config.consumedServices.mockId.isValid).is.false;
+        });
+
+        it ('should set default service.requestMethod if custom config does not have a valid property', () => {
+            customConfig.consumedServices.mockId.requestMethod = undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.requestMethod).is.not.undefined;
+            expect(config.consumedServices.mockId.requestMethod).is.equal(defaultConfig.consumedServices.defaultServiceId.requestMethod)
+        });
+
+        it ('should set service.isValid=False if custom config does not have a valid requestMethod', () => {
+            customConfig.consumedServices.mockId.requestMethod = 'GET_MOCK';
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.isValid).is.false;
+        });
+
+        it ('should accept requestMethod without any case sensitive restrictions', () => {
+            customConfig.consumedServices.mockId.requestMethod = 'get';
+            let config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.requestMethod).is.equal('GET');
+            expect(config.consumedServices.mockId.isValid).is.true;
+
+            customConfig.consumedServices.mockId.requestMethod = 'Get';
+            config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.requestMethod).is.equal('GET');
+            expect(config.consumedServices.mockId.isValid).is.true;
+
+            customConfig.consumedServices.mockId.requestMethod = 'gEt';
+            config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.requestMethod).is.equal('GET');
+            expect(config.consumedServices.mockId.isValid).is.true;
+        });
+
+        it ('should set default service.expectedResponseStatus if custom config does not have a valid property', () => {
+            customConfig.consumedServices.mockId.expectedResponseStatus = undefined;
+            const config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.expectedResponseStatus).is.not.undefined;
+            expect(config.consumedServices.mockId.expectedResponseStatus).is.equal(defaultConfig.consumedServices.defaultServiceId.expectedResponseStatus)
+        });
+
+        it ('should set default service.isRequired if custom config does not have a valid property', () => {
+            customConfig.consumedServices.mockId.isRequired = undefined;
+            let config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.isRequired).is.not.undefined;
+            expect(config.consumedServices.mockId.isRequired).is.equal(defaultConfig.consumedServices.defaultServiceId.isRequired)
+
+            customConfig.consumedServices.mockId.isRequired = "ABC";
+            config = configValidator(customConfig);
+            expect(config.consumedServices.mockId.isRequired).is.not.equals("ABC");
+            expect(config.consumedServices.mockId.isRequired).is.equal(defaultConfig.consumedServices.defaultServiceId.isRequired)
+        });
+    });
+
+    describe("should validate api configurations", () => {
+        it ('should set default config if custom config does not have valid apis config', () => {
+            customConfig.apis = undefined;
+            const config = configValidator(customConfig);
+            expect(config.apis).is.not.undefined;
+            expect(config.apis).is.empty
+        });
+
+        it ('should set default apiName if custom config does not have valid property', () => {
+            customConfig.apis = { mockId: {}};
+            const config = configValidator(customConfig);
+            expect(config.apis.mockId.apiName).is.not.undefined;
+            expect(config.apis.mockId.apiName).is.equal(defaultConfig.apis.defaultApi.apiName)
+        });
+
+        it ('should set default requestMethod if custom config does not have valid property', () => {
+            customConfig.apis = { mockId: {}};
+            let config = configValidator(customConfig);
+            expect(config.apis.mockId.requestMethod).is.not.undefined;
+            expect(config.apis.mockId.requestMethod).is.equal(defaultConfig.apis.defaultApi.requestMethod)
+
+            customConfig.apis = { mockId: { requestMethod: "GET_UPPER" }};
+            config = configValidator(customConfig);
+            expect(config.apis.mockId.requestMethod).is.not.equal("GET_UPPER");
+            expect(config.apis.mockId.requestMethod).is.equal(defaultConfig.apis.defaultApi.requestMethod)
+        });
+
+        it ('should set default dependsOn config if custom config does not have valid property', () => {
+            customConfig.apis = { mockId: {}};
+            let config = configValidator(customConfig);
+            expect(config.apis.mockId.dependsOn).is.not.undefined;
+            expect(config.apis.mockId.dependsOn).is.instanceOf(Array)
+
+            customConfig.apis = { mockId: { dependsOn: {}}};
+            config = configValidator(customConfig);
+            expect(config.apis.mockId.dependsOn).is.instanceOf(Array)
+
+        });
+    });
 });
